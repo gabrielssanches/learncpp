87ee2829c73ed32a3b8ca83b576193bb6975e6a9
Gabriel dos Santos Sanches <gasa@prevas.dk>
2023-08-13 16:06:31 +0200

    wip

--- aoc_2015_3_raylib/main.cpp
+++ aoc_2015_3_raylib/main.cpp
@@ -6,6 +6,10 @@
 #include <tuple>
 #include <string>
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
 using namespace std;
 
 class Matrix2D {
@@ -131,15 +135,30 @@ int main(int argc, char *argv[]) {
     int screenWidth = 800;
     int screenHeight = 600;
 
-    raylib::Color textColor(BLACK);
     raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    raylib::Color house_rect(0, 68, 130); 
-    raylib::Color grid_color(0, 68, 130); 
 
-    SetTargetFPS(60);
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(__window_main_loop, 0, 1);
+#else
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!window.ShouldClose()) {    // Detect window close button or ESC key
+        __window_main_loop();
+    }
+#endif
+    return 0;
+}
+
+static void __window_main_loop(void) {
+    static bool setup_done = false;
+
+    if (setup_done) {
+        goto main_loop;
+    }
 
     int rot = 0;
-    
     float window_ts = 0.0f;
     float lastdrawn_ts = 0.0f;
 
@@ -150,133 +169,130 @@ int main(int argc, char *argv[]) {
 
     raylib::Camera2D cam(raylib::Vector2{0,0}, raylib::Vector2{0,0}, 0.0f, 1.0f);
 
-    while (!w.ShouldClose()) {// Detect window close button or ESC key
-        window_ts += w.GetFrameTime();
+main_loop:
+    window_ts += w.GetFrameTime();
 
-        BeginDrawing();
+    BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+    ClearBackground(RAYWHITE);
 
-        if (IsKeyDown(KEY_UP)) {
-            //cam.target = cam.GetTarget() + Vector2{0, -10};
-            raylib::Vector2 target_diff(0, -10);
-            raylib::Vector2 target;
-            target = cam.GetTarget();
-            target += target_diff;
-            cam.SetTarget(target); // Addition assignment operator override.
-        }
-        if (IsKeyDown(KEY_DOWN)) {
-            //cam.target = cam.target + Vector2{0, 10};
-            // raylib-cpp
-            raylib::Vector2 target_diff(0, 10);
-            raylib::Vector2 target;
-            target = cam.GetTarget();
-            target += target_diff;
-            cam.SetTarget(target); // Addition assignment operator override.
-        }
-
-
-        BeginMode2D(cam);
-
-        if (true || (window_ts - lastdrawn_ts) > 0.010f) {
-            lastdrawn_ts = window_ts;
-            rot+=10;
-
-            if (!input_f.get(c)) {
-                cout << "DONE!" << endl;
-                break;
-            }
-            santa.move(c);
-            houses.gift(santa.pos());
-        }
-        
-        vector<vector <int>> *grid = houses.grid_get(0);
-        int row_i = 0;
-        for (vector<int> &row : *grid) {
-            int col_i = 0;
-            for (int col : row) {
-                raylib::Rectangle h_rect(screenWidth/2 + (row_i * 10), screenHeight/2 -10 - (col_i * 10), 10, 10);
-                if (col > 0) {
-                    h_rect.Draw(GREEN);
-                } else {
-                    h_rect.Draw(LIGHTGRAY);
-                }
-                col_i++;
-            }
-            row_i++;
-        }
-        grid = houses.grid_get(1);
-        row_i = 0;
-        for (vector<int> &row : *grid) {
-            int col_i = 0;
-            for (int col : row) {
-                raylib::Rectangle h_rect(screenWidth/2 + (row_i * 10), screenHeight/2 + (col_i * 10), 10, 10);
-                if (col > 0) {
-                    h_rect.Draw(GREEN);
-                } else {
-                    h_rect.Draw(LIGHTGRAY);
-                }
-                col_i++;
-            }
-            row_i++;
-        }
-        grid = houses.grid_get(2);
-        row_i = 0;
-        for (vector<int> &row : *grid) {
-            int col_i = 0;
-            for (int col : row) {
-                raylib::Rectangle h_rect(screenWidth/2 -10 - (row_i * 10), screenHeight/2 + (col_i * 10), 10, 10);
-                if (col > 0) {
-                    h_rect.Draw(GREEN);
-                } else {
-                    h_rect.Draw(LIGHTGRAY);
-                }
-                col_i++;
-            }
-            row_i++;
-        }
-        grid = houses.grid_get(3);
-        row_i = 0;
-        for (vector<int> &row : *grid) {
-            int col_i = 0;
-            for (int col : row) {
-                raylib::Rectangle h_rect(screenWidth/2 -10 - (row_i * 10), screenHeight/2 -10 - (col_i * 10), 10, 10);
-                if (col > 0) {
-                    h_rect.Draw(GREEN);
-                } else {
-                    h_rect.Draw(LIGHTGRAY);
-                }
-                col_i++;
-            }
-            row_i++;
-        }
-
-        int x, y;
-        tie(x, y) = santa.pos();
-        raylib::Rectangle santa(screenWidth/2 + (x * 10), screenHeight/2 -10 - (y * 10), 10, 10);
-        santa.Draw(RED);
-
-        raylib::Vector2 y_axis(screenWidth/2, 0);
-        y_axis.DrawLine((raylib::Vector2){screenWidth/2, screenHeight}, (raylib::Color){BLACK});
-        raylib::Vector2 x_axis(0, screenHeight/2);
-        x_axis.DrawLine((raylib::Vector2){screenWidth, screenHeight/2}, (raylib::Color){BLACK});
-
-        //santa.Draw((raylib::Vector2){0,0}, (float)rot, RED);
-
-        //house_rect.DrawLine(screenWidth/2100, 100, 200, 200);
-        
-        //house_rect.DrawRectangle(-100, screenHeight/2 - 128, 256, 256);
-        string pos_str;
-        pos_str = "Santa(" + to_string(x) + ", " + to_string(y) + ")";
-        textColor.DrawText(pos_str, 10, 10, 20);
-        EndMode2D();
-        EndDrawing();
+    if (IsKeyDown(KEY_UP)) {
+        //cam.target = cam.GetTarget() + Vector2{0, -10};
+        raylib::Vector2 target_diff(0, -10);
+        raylib::Vector2 target;
+        target = cam.GetTarget();
+        target += target_diff;
+        cam.SetTarget(target); // Addition assignment operator override.
+    }
+    if (IsKeyDown(KEY_DOWN)) {
+        //cam.target = cam.target + Vector2{0, 10};
+        // raylib-cpp
+        raylib::Vector2 target_diff(0, 10);
+        raylib::Vector2 target;
+        target = cam.GetTarget();
+        target += target_diff;
+        cam.SetTarget(target); // Addition assignment operator override.
     }
 
-    unsigned int gifted_houses = houses.gifted_houses();
-    cout << "Part 1: Gifted Houses = " << gifted_houses << endl;
 
-    input_f.close();
+    BeginMode2D(cam);
 
-    return 0;
+    if (true || (window_ts - lastdrawn_ts) > 0.010f) {
+        lastdrawn_ts = window_ts;
+        rot+=10;
+
+        if (!input_f.get(c)) {
+            cout << "DONE!" << endl;
+            break;
+        }
+        santa.move(c);
+        houses.gift(santa.pos());
+    }
+    
+    vector<vector <int>> *grid = houses.grid_get(0);
+    int row_i = 0;
+    for (vector<int> &row : *grid) {
+        int col_i = 0;
+        for (int col : row) {
+            raylib::Rectangle h_rect(screenWidth/2 + (row_i * 10), screenHeight/2 -10 - (col_i * 10), 10, 10);
+            if (col > 0) {
+                h_rect.Draw(GREEN);
+            } else {
+                h_rect.Draw(LIGHTGRAY);
+            }
+            col_i++;
+        }
+        row_i++;
+    }
+    grid = houses.grid_get(1);
+    row_i = 0;
+    for (vector<int> &row : *grid) {
+        int col_i = 0;
+        for (int col : row) {
+            raylib::Rectangle h_rect(screenWidth/2 + (row_i * 10), screenHeight/2 + (col_i * 10), 10, 10);
+            if (col > 0) {
+                h_rect.Draw(GREEN);
+            } else {
+                h_rect.Draw(LIGHTGRAY);
+            }
+            col_i++;
+        }
+        row_i++;
+    }
+    grid = houses.grid_get(2);
+    row_i = 0;
+    for (vector<int> &row : *grid) {
+        int col_i = 0;
+        for (int col : row) {
+            raylib::Rectangle h_rect(screenWidth/2 -10 - (row_i * 10), screenHeight/2 + (col_i * 10), 10, 10);
+            if (col > 0) {
+                h_rect.Draw(GREEN);
+            } else {
+                h_rect.Draw(LIGHTGRAY);
+            }
+            col_i++;
+        }
+        row_i++;
+    }
+    grid = houses.grid_get(3);
+    row_i = 0;
+    for (vector<int> &row : *grid) {
+        int col_i = 0;
+        for (int col : row) {
+            raylib::Rectangle h_rect(screenWidth/2 -10 - (row_i * 10), screenHeight/2 -10 - (col_i * 10), 10, 10);
+            if (col > 0) {
+                h_rect.Draw(GREEN);
+            } else {
+                h_rect.Draw(LIGHTGRAY);
+            }
+            col_i++;
+        }
+        row_i++;
+    }
+
+    int x, y;
+    tie(x, y) = santa.pos();
+    raylib::Rectangle santa(screenWidth/2 + (x * 10), screenHeight/2 -10 - (y * 10), 10, 10);
+    santa.Draw(RED);
+
+    raylib::Vector2 y_axis(screenWidth/2, 0);
+    y_axis.DrawLine((raylib::Vector2){screenWidth/2, screenHeight}, (raylib::Color){BLACK});
+    raylib::Vector2 x_axis(0, screenHeight/2);
+    x_axis.DrawLine((raylib::Vector2){screenWidth, screenHeight/2}, (raylib::Color){BLACK});
+
+    //santa.Draw((raylib::Vector2){0,0}, (float)rot, RED);
+
+    //house_rect.DrawLine(screenWidth/2100, 100, 200, 200);
+    
+    //house_rect.DrawRectangle(-100, screenHeight/2 - 128, 256, 256);
+    string pos_str;
+    pos_str = "Santa(" + to_string(x) + ", " + to_string(y) + ")";
+    raylib::Color textColor(BLACK);
+    textColor.DrawText(pos_str, 10, 10, 20);
+    EndMode2D();
+    EndDrawing();
+
+    //unsigned int gifted_houses = houses.gifted_houses();
+    //cout << "Part 1: Gifted Houses = " << gifted_houses << endl;
+    //input_f.close();
 }
